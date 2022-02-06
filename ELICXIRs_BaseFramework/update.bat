1c7b640fdf51fd0f19876e14e9be572c90640f67
ELICXIR <elicxir536329@outlook.jp>
2022-02-06 11:46:00 +0900

    Update update.bat

--- ELICXIRs_BaseFramework/update.bat
+++ ELICXIRs_BaseFramework/update.bat
@@ -1,4 +1,5 @@
 git submodule foreach git pull origin main
+robocopy Assets\BaseSystem\Assets\BaseSystem_Customize Assets\BaseSystem_Customize /XO
 del /q Assets\BaseSystem\Assets\BaseSystem_Customize.meta
 del /q Assets\BaseSystem\Assets\BaseSystem_Customize
 pause
